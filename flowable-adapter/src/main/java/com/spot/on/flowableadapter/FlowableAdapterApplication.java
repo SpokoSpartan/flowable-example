6313528c58ebc3f76d4cb4467718e079730e139a
SpokoSpartan <wojtekspoton@gmail.com>
2022-05-16 20:22:00 +0200

    Init flowable-adapter app

---
+++ flowable-adapter/src/main/java/com/spot/on/flowableadapter/FlowableAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.spot.on.flowableadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableAdapterApplication.class, args);
+	}
+
+}
---
+++ flowable-adapter/src/test/java/com/spot/on/flowableadapter/FlowableAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spot.on.flowableadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
